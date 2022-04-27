eb326bc537f0fc04780561bc9a7a83e7071a7d57
Toyemakinwa <toyemakinwa@yahoo.com>
2022-04-27 13:55:35 +0300

    to do list
    If committed, this should create a to do list

---
+++ js/script.js
@@ -0,0 +1,40 @@
+function newItem(){
+    //Add a new item to list of items using jquery
+    
+    //this creates a list element 
+    let li = $('<li></li>');
+    //creates a new input value
+    let inputValue = $('#input').val();
+    //appends it as a child to the list
+    li.append(inputValue);
+    
+      
+    //create a conditional statement to input text
+       if (inputValue === '') {
+         alert('You must write something!');
+       } else {
+           // this returns back the form if  othing is typed
+         $('#list').append(li);
+       }
+
+    // strikes list item out when clicked
+    li.on('dblclick', function crossOut(){
+      li.toggleClass('strike');
+    });
+    
+    //creating a crossout button to delete
+    let crossOutButton= $('<crossOutButton></crossOutButton>');
+    //append the X button
+    crossOutButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
+    
+    //when clicked clear out the list item
+    crossOutButton.on('click', deleteListItem);
+
+     function deleteListItem(){
+        li.addClass("delete");
+     }
+
+    //for sorting the list item when dragged
+     $('#list').sortable();
+    }
